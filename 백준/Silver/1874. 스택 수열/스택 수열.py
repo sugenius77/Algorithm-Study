cfde159210fb36d9713a91e360903edc7bfba98a
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-06-29 14:48:09 +0900

    Update 스택 수열.py

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,15 +1,18 @@
 import sys
 n = int(sys.stdin.readline())
 
-i = 1
+current_num = 1
 stack = []
+# +,-를 삽입 할 배열
 answer = []
+# 수열 만드는게 불가능한지 아닌지 확인하는 Flag
 flag = True
+
 for _ in range(n):
   x = int(sys.stdin.readline())
-  while i <= x:
-    stack.append(i)
-    i += 1
+  while current_num <= x:
+    stack.append(current_num)
+    current_num += 1
     answer.append('+')
   if stack[-1] == x:
     stack.pop()
