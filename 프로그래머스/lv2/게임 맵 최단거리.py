17c256f15fe343106ec4f37f8ff0788c6825d2db
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-05 17:26:22 +0900

    Update 게임 맵 최단거리.py

--- 프로그래머스/lv2/게임 맵 최단거리.py
+++ 프로그래머스/lv2/게임 맵 최단거리.py
@@ -1,17 +1,17 @@
 from collections import deque
 def solution(maps):
-    visited = [[0] * (len(maps[0])) for _ in range(len(maps))]
-    return bfs(0,0,maps,visited)
-def bfs(x,y,maps,visited):
     # 동,서,남,북
     dx = [0,0,1,-1]
     dy = [1,-1,0,0]
     # 행 n, 열 m
     n = len(maps)
-    m = len(maps[0])        
+    m = len(maps[0])
+    # 거리를 저장
+    visited = [[0] * m for _ in range(n)]
     q = deque()
-    q.append((x,y))
-    visited[x][y] = 1
+    # (0,0) 에서 시작
+    q.append((0,0))
+    visited[0][0] = 1
     while q:
         x,y = q.popleft()
         for i in range(4):
@@ -21,6 +21,4 @@ def bfs(x,y,maps,visited):
                 q.append((nx,ny))
                 visited[nx][ny] = visited[x][y] + 1
                 maps[nx][ny] = 0
-                
-
     return visited[-1][-1] if visited[-1][-1] else -1
