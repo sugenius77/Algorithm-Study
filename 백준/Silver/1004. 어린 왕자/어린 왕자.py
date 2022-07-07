f0e8628d61bbb42978a10cc8c6b8a0ab39bcd786
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-07 14:35:19 +0900

    [Silver III] Title: 어린 왕자, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+  cnt = 0
+  x1, y1, x2, y2 = map(int,sys.stdin.readline().split())
+  n = int(sys.stdin.readline())
+  for i in range(n):
+    cx, cy, r = map(int,sys.stdin.readline().split())
+    dist1 = ((cx - x1) **2 + (cy - y1) **2) ** 0.5
+    dist2 = ((cx - x2) **2 + (cy - y2) **2) ** 0.5
+    if dist1 > r and dist2 < r:
+      cnt += 1
+    if dist2 > r and dist1 < r:
+      cnt += 1
+  print(cnt)
+
+
+# 두 점 사이의 거리보다 지름이 크면 포함되어 있는 것
