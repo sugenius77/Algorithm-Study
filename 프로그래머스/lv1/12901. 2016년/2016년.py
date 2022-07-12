43c2aeac0e16b202a0204bd8ea02c13df5733099
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-12 14:12:35 +0900

    Update 2016년.py

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -2,17 +2,10 @@ def solution(a, b):
     day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
     end_date = [31,29,31,30,31,30,31,31,30,31,30,31]
     
-    # 3의 1일의 요일
+    # 입력 날짜까지의 일수 계산
     n = sum(end_date[:a-1]) + b
     
-    aa = n % 7
-    answer = day[aa-1]
-    # 3월 24일 요일
-    # tmp  = 7  * (b // 7) + 1
-    
-#     print(n,tmp)
-#     answer = day[day.index(day[n]) + (b - tmp)]
-    # answer = 0
-    return answer
+    # 요일 구하기
+    idx = n % 7
 
-# print(solution(3,31))
+    return day[idx-1]
