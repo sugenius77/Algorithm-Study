bf62b2f6a5866e4130d54ecef2dc3e3b182754f3
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-14 12:55:37 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,10 +1,3 @@
-'''
-상하좌우 이동
-왼손: 1,4,7
-오른손: 3,6,9
-가까운: 2,5,8,0
-거리가 같다면 왼손잡이 , 오른손 잡이
-'''
 def solution(numbers, hand):
     answer = ''
     left_now = [3,0]
@@ -14,12 +7,15 @@ def solution(numbers, hand):
              7:[2,0], 8:[2,1], 9:[2,2],
              '*':[3,0], 0:[3,1], '#':[3,2]}
     for i in numbers:
+        # 무조건 왼쪽 엄지
         if i in [1,4,7]:
             answer = answer + 'L'
             left_now = keypad[i]
+        # 무조건 오른쪽 엄지
         if i in [3,6,9]:
             answer = answer + 'R'
             right_now = keypad[i]
+        # 거리에 따른 손가락
         if i in [2,5,8,0]:    
             left = abs(keypad[i][0] - left_now[0]) + abs(keypad[i][1] - left_now[1])
             right = abs(keypad[i][0] - right_now[0]) + abs(keypad[i][1] - right_now[1])
@@ -29,6 +25,7 @@ def solution(numbers, hand):
             elif left > right:
                 right_now = keypad[i]
                 answer = answer + 'R'
+            # 거리가 같으면 왼/오른손잡이
             elif left == right:
                 if hand == 'right':
                     right_now = keypad[i]
@@ -36,5 +33,4 @@ def solution(numbers, hand):
                 else:
                     left_now = keypad[i]
                     answer = answer + 'L'
-    
     return answer
