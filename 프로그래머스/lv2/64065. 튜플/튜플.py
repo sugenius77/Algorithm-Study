ceb6811148afaf0e41c279ac083384d00ffe0931
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-22 22:09:50 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -2,17 +2,22 @@ from collections import defaultdict
 def solution(s):
     answer = []
     dict = defaultdict(int)
+    tmp = []
+    # 문자열 파싱
     new_s = s.split('}')[:-2]
     for i in new_s:
-        answer.append(i[2:].split(','))
-
-    for i in answer:
+        tmp.append(i[2:].split(','))
+    
+    # 갯수 세기
+    for i in tmp:
         for j in i:
             dict[j] += 1
-            
+    
+    # 딕셔너리 value 값을 기준으로 정렬
     result = sorted(dict.items(),key = lambda x :x[1],reverse = True)
-    res = []
+    
+    # 키 값만 리스트에 넣기
     for i in result:
-        res.append(int(i[0]))
+        answer.append(int(i[0]))
         
-    return res
+    return answer
