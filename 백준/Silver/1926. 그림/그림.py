24646a80624ac6e8d18dddca95174a0035b1aae3
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-07-29 00:38:42 +0900

    [Silver I] Title: 그림, Time: 544 ms, Memory: 33876 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,19 +1,25 @@
 import sys
+from collections import deque
 
 # 행,열
 n, m = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
+
+# 그림의 개수
 num = 0
+
+# 가장 큰 그림 = 1의 개수가 많은 것
 max_cnt = 0
+
 dx = [0, 1, -1, 0]  
 dy = [1, 0, 0, -1]
-from collections import deque
 
-
+# bfs 구현
 def bfs(x, y):
     q = deque()
     q.append([x, y])
+    # 1을 세주는 cnt
     cnt = 1
     visited[x][y] = True
     while q:
@@ -31,9 +37,10 @@ def bfs(x, y):
 
 for i in range(n):
   for j in range(m):
+    # 값이 1이고 방문하지 않았다면
     if graph[i][j] == 1 and not visited[i][j]:
       num += 1     
       max_cnt = max(max_cnt,bfs(i,j))
+      
 print(num)    
 print(max_cnt)
-
