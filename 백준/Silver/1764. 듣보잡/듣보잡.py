e3f19c78ffd26fcc96f575f76d9fabbb5c17ddc2
sugenius77 <68172999+sugenius77@users.noreply.github.com>
2022-09-03 17:11:59 +0900

    [Silver IV] Title: 듣보잡, Time: 132 ms, Memory: 42108 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -9,12 +9,19 @@ never_seen = set()
 # 듣도 못한 사람들
 for _ in range(n):
   never_heard.add(sys.stdin.readline().strip())
-# 보도 못한 사람의 수
+  
+# 보도 못한 사람들
 for _ in range(m):
   never_seen.add(sys.stdin.readline().strip())
 
+# 듣도 보도 못한 사람들 = 두 집합의 교집합
 result = never_heard.intersection(never_seen)
+
+# 사전 순으로 정렬
 result = sorted(result)
+
+# 사람의 수 출력
 print(len(result))
+# 한 사람 씩 출력
 for n in result:
   print(n)
