e28b9bb2cfb713a81127af04ab9537a409c22c70
Sujin lee <68172999+sugenius77@users.noreply.github.com>
2022-09-16 15:59:35 +0900

    Update 가장 긴 팰린드롬.py

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -4,6 +4,4 @@ def solution(s):
         for j in range(len(s),i,-1):
             if s[i:j+1] == s[i:j+1][::-1]:
                 answer = max(answer,len(s[i:j+1]))
-            
-                # print(s[i:j+1])
     return answer
