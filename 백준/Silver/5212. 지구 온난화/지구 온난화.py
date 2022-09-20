b8b7feab7b6d330a556b3b38464d3c3d8ec33194
Sujin lee <zziinn812@gmail.com>
2022-09-20 17:01:43 +0900

    [Silver II] Title: 지구 온난화, Time: 104 ms, Memory: 32508 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -36,37 +36,63 @@ if land_cnt == 0:
   print('X')
 else:
 # 모든 섬을 포함하는 가장 작은 직사각형으로 지도의 크기 자르기
-  ## 필요없는 영역 자르기
-  result = deque(result)
-  while( 'X' not in result[0] ):
-      result.popleft()
-  while( 'X' not in result[-1]):
-      result.pop()
-    
-  start = 0
-  end = len(result[0]) - 1
-  
-  # 플래그 이용
-  # 50년 후 지도 시작 부분
-  flag = True
-  while flag:
-    for i in result:
-      if i[start] == 'X':
-        flag = False
-        break
-    else:
-      start += 1
-        
-  # 50년 후 지도 끝나는 부분
-  flag = True  
-  while flag:
-    for i in result:
-      if i[end] == 'X':
-        flag = False
-        break 
-    else:  
-      end  -= 1
-  
+  answer = []
+  # for i in result:
+  #   # print(i)
+  #   if 'X' in i:
+  #     print(i)
+  #     answer.append(i)
+  # # print(answer)
+  # # # 필요없는 영역 자르기
+  # # result = deque(result)
+  # # while( 'X' not in result[0] ):
+  # #     result.popleft()
+  # # while( 'X' not in result[-1]):
+  # #     result.pop()
+  # # print(result)
 
-  for i in result:
-    print(''.join(i[start:end+1]))
+  # start = 0
+  # end = len(result[0]) - 1
+  
+  # # 플래그 이용
+  # # 50년 후 지도 시작 부분
+  # flag = True
+  # while flag:
+  #   for i in result:
+  #     if i[start] == 'X':
+  #       flag = False
+  #       break
+  #   else:
+  #     start += 1
+        
+  # # 50년 후 지도 끝나는 부분
+  # flag = True  
+  # while flag:
+  #   for i in result:
+  #     if i[end] == 'X':
+  #       flag = False
+  #       break 
+  #   else:  
+  #     end  -= 1
+  
+  startR = 0
+  endR = 0
+  for i in range(R):
+    if 'X' in result[i]:
+      startR = i
+      break
+  for i in range(R-1,-1,-1):
+    if 'X' in result[i]:
+      endR = i
+      break
+  
+  idx = []
+  for j in range(C):
+    for i in range(startR, endR + 1):
+      if 'X' == result[i][j]:
+        idx.append(j)
+        break
+
+  for i in result[startR:endR+1]:
+    print(''.join(i[idx[0]:idx[-1]+1]))
+  #   print(''.join(i[start:end+1]))
