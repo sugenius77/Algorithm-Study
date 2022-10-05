1af10d63446ef029924a539c68c0125a0e22495b
Sujin lee <zziinn812@gmail.com>
2022-10-05 14:07:17 +0900

    [Gold IV] Title: 연구소, Time: 2736 ms, Memory: 32508 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,27 +1,36 @@
 import sys
 from collections import deque
+from itertools import combinations
 import copy
 
 n,m = map(int,sys.stdin.readline().split())
 
 graph = []
+virus_zone = []
+safe_zone = []
 for i in range(n):
   graph.append(list(map(int,sys.stdin.readline().split())))
 
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-q = deque()
+
 ans = 0
 
-def bfs():
+# 벽을 3개 세울 때 중복되는 조합 없게 하기 위해 combination 사용
+# ex) (1,1) (2,4) (3,5) 세우고 검사한 뒤 나중에 (2,4) (1,1) (3,5)를 방지하기 위해
+for i in range(n):
+  for j in range(m):
+    if graph[i][j] == 2:
+      virus_zone.append([i,j])
+    if graph[i][j] == 0:
+      safe_zone.append([i,j])
+
+
+def bfs(tmp_graph):
   global ans
-  tmp = copy.deepcopy(graph)
-  # 2인 곳에서 바이러스 퍼지기 시작
-  for i in range(n):
-    for j in range(m):
-      if tmp[i][j] == 2:
-        q.append([i,j])
+  q = deque(virus_zone)
+
   # 상하좌우가 0이라면 2로 바꾸기 = 바이러스 퍼지는 중
   while q:
     x,y = q.popleft()
@@ -29,26 +38,33 @@ def bfs():
       nx = x + dx[i]
       ny = y + dy[i]
       if 0 <= nx < n and 0 <= ny < m:
-        if tmp[nx][ny] == 0:
-          tmp[nx][ny] = 2
+        if tmp_graph[nx][ny] == 0:
+          tmp_graph[nx][ny] = 2
           q.append([nx,ny])
   # 0의 개수 세기
   cnt = 0
-  for i in tmp:
+  for i in tmp_graph:
     cnt += i.count(0)
   ans = max(cnt,ans)
 
 # 백트래킹을 이용해서 벽을 세우는 경우의 수 구하기
-def make_wall(x):
-  if x == 3:
-    bfs()
-    return 
-  for i in range(n):
-    for j in range(m):
-      if graph[i][j] == 0:
-        graph[i][j] = 1
-        make_wall(x+1)
-        graph[i][j] = 0
+def make_wall():
+  safe_zones_combi = combinations(safe_zone, 3)
+  
+  for walls in safe_zones_combi:
+      a, b, c = walls[0], walls[1], walls[2]
 
-make_wall(0)
+      graph[a[0]][a[1]] = 1
+      graph[b[0]][b[1]] = 1
+      graph[c[0]][c[1]] = 1
+
+      tmp_graph = [item[:] for item in graph]
+
+      bfs(tmp_graph)
+
+      graph[a[0]][a[1]] = 0
+      graph[b[0]][b[1]] = 0
+      graph[c[0]][c[1]] = 0
+
+make_wall()
 print(ans)
