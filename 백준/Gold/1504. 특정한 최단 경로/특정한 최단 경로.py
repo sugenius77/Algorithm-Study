44c772c9126c520fde6c4aa6ded53bd36ae2ceb1
Sujin lee <zziinn812@gmail.com>
2022-10-06 15:36:35 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 488 ms, Memory: 62892 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,9 +1,3 @@
-'''
-1. 1번 정점부터 n번 정점으로 최단 거리로 이동
-2. 임의로 주어진 두 정점은 반드시 통과
-3. 한번 이동했던 정점, 간선 다시 이동 가능
-'''
-
 import sys
 
 # 정점 n ,간선 e
@@ -21,8 +15,8 @@ for i in range(e):
 # 반드시 거쳐야하는 서로 다른 두 개의 정점
 v1,v2 = map(int,sys.stdin.readline().split())
 
-INF = 1e9
 
+INF = 1e9
 
 import heapq
 def dijkstra(start):
@@ -47,7 +41,6 @@ original_dist = dijkstra(1)
 v1_dist = dijkstra(v1)
 v2_dist = dijkstra(v2)
 
-
 # 두가지 방법
 # 시작점 -> v -> vv -> n
 # 시작점 -> vv -> v -> n
@@ -57,4 +50,3 @@ v2_v1 = original_dist[v2] + v2_dist[v1] + v1_dist[n]
 ans = min(v1_v2,v2_v1)
 
 print(ans if ans < INF else -1)
-
