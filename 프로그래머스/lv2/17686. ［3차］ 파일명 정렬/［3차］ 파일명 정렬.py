e98b733bac41094dd3ca517ce3d2346c728a5636
Sujin lee <zziinn812@gmail.com>
2022-11-17 11:37:00 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.18 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,21 @@
+def solution(files):
+    tmp = []
+    head, number, tail = '', '', ''
+    
+    for file in files:
+        for i in range(len(file)):
+            if file[i].isdigit():
+                head = file[:i]
+                number = file[i:]
+                
+                for j in range(len(number)):
+                    if not number[j].isdigit():
+                        tail = number[j:]
+                        number = number[:j]
+                        break
+                tmp.append([head,number,tail])
+                head, number, tail = '','',''
+                break
+    tmp.sort(key = lambda x:(x[0].lower(), int(x[1])))
+
+    return ["".join(i) for i in tmp]
